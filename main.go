f4eced6c9dc15e8317700e2448498eda7d7144e8
frank <www.417329062@qq.com>
2021-05-24 14:27:52 +0800

    update token transfer options

--- main.go
+++ main.go
@@ -70,6 +70,11 @@ var sendETHCmd = &cli.Command{
 			Value: 21000, // in units
 			Usage: "the amount of gas limit (wei)",
 		},
+		&cli.Uint64Flag{
+			Name:  "gasPrice",
+			Value: 0, // in units
+			Usage: "the amount of gas price (wei)",
+		},
 	},
 	Action: func(c *cli.Context) error {
 		endpoint := os.Getenv("ENDPOINT")
@@ -77,7 +82,7 @@ var sendETHCmd = &cli.Command{
 			endpoint = defaultEndPoint
 		}
 
-		return PayEth(endpoint, c.String("fromKey"), c.String("fromCode"), c.String("toKey"), c.Int64("amount"), c.Uint64("gasLimit"))
+		return PayEth(endpoint, c.String("fromKey"), c.String("fromCode"), c.String("toKey"), c.Int64("amount"), c.Uint64("gasLimit"), c.Uint64("gasPrice"))
 	},
 }
 
@@ -113,6 +118,11 @@ var sendBZZCmd = &cli.Command{
 			Value: 0,
 			Usage: "the amount of gas limit (wei)",
 		},
+		&cli.Uint64Flag{
+			Name:  "gasPrice",
+			Value: 0, // in units
+			Usage: "the amount of gas price (wei)",
+		},
 	},
 	Action: func(c *cli.Context) error {
 		endpoint := os.Getenv("ENDPOINT")
@@ -120,12 +130,12 @@ var sendBZZCmd = &cli.Command{
 			endpoint = defaultEndPoint
 		}
 
-		return PayBzz(endpoint, c.String("fromKey"), c.String("fromCode"), c.String("toKey"), c.Int64("amount"), c.Uint64("gasLimit"))
+		return PayBzz(endpoint, c.String("fromKey"), c.String("fromCode"), c.String("toKey"), c.Int64("amount"), c.Uint64("gasLimit"), c.Uint64("gasPrice"))
 	},
 }
 
 // PayEth 传入出账的私钥，密码（geth导入账号那部分），传入要进账的公钥，金额单位是wei * 10的9次方。
-func PayEth(endpoint, fromKey, fromCode, toKey string, amount int64, gasLimit uint64) error {
+func PayEth(endpoint, fromKey, fromCode, toKey string, amount int64, gasLimit, gasPrice uint64) error {
 	client, err := ethclient.Dial(endpoint)
 	if err != nil {
 		return err
@@ -154,16 +164,20 @@ func PayEth(endpoint, fromKey, fromCode, toKey string, amount int64, gasLimit ui
 
 	// 支付的金额。
 	value := big.NewInt(amount * 100000000000000) // in wei (0.000000001 eth)
-	gasPrice, err := client.SuggestGasPrice(context.Background())
+	price, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return err
 	}
 
+	if big.NewInt(int64(gasPrice)).Cmp(price) > 0 {
+		price = big.NewInt(int64(gasPrice))
+	}
+
+	log.Println("gas Price:", price)
 	log.Println("gas Limit:", gasLimit)
 
 	toAddress := common.HexToAddress(toKey)
-	// 这个方法已弃用，新的我不会用
-	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, nil)
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, price, nil)
 
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
@@ -186,7 +200,7 @@ func PayEth(endpoint, fromKey, fromCode, toKey string, amount int64, gasLimit ui
 }
 
 // PayBzz 传入出账的私钥，密码（geth导入账号那部分），传入要进账的公钥，金额单位是wei * 10的9次方。
-func PayBzz(endpoint, fromKey, fromCode, toKey string, amount int64, gasLimit uint64) error {
+func PayBzz(endpoint, fromKey, fromCode, toKey string, amount int64, gasLimit, gasPrice uint64) error {
 	client, err := ethclient.Dial(endpoint)
 	if err != nil {
 		return err
@@ -215,9 +229,13 @@ func PayBzz(endpoint, fromKey, fromCode, toKey string, amount int64, gasLimit ui
 
 	// 代币传输不需要传输ETH，因此将交易“值”设置为“0”。
 	value := big.NewInt(0)
-	gasPrice, err := client.SuggestGasPrice(context.Background())
+	price, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return err
+	}
+
+	if big.NewInt(int64(gasPrice)).Cmp(price) > 0 {
+		price = big.NewInt(int64(gasPrice))
 	}
 
 	toAddress := common.HexToAddress(toKey)
@@ -241,20 +259,23 @@ func PayBzz(endpoint, fromKey, fromCode, toKey string, amount int64, gasLimit ui
 	data = append(data, paddedAddress...)
 	data = append(data, paddedAmount...)
 
-	if gasLimit <= 0 {
-		// 获取燃气上限制
-		gasLimit, err = client.EstimateGas(context.Background(), ethereum.CallMsg{
-			To:   &toAddress,
-			Data: data,
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
+	// 获取燃气上限制
+	gLimit, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
+		To:   &toAddress,
+		Data: data,
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	if gLimit > gasLimit {
+		gasLimit = gLimit
+	}
+
+	log.Println("gas Price:", price)
 	log.Println("gas Limit:", gasLimit)
 
-	tx := types.NewTransaction(nonce, tokenAddress, value, gasLimit, gasPrice, data)
+	tx := types.NewTransaction(nonce, tokenAddress, value, gasLimit, price, data)
 
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
